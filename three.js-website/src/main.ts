435ae5740f2e090c24a9ef6dbc8bd8368f35b287
kokoro <cocosyu.11261126@gmail.com>
2022-08-15 21:26:09 +0900

    Add: リサイズ処理を追加

--- three.js-website/src/main.ts
+++ three.js-website/src/main.ts
@@ -33,16 +33,16 @@ renderer.setPixelRatio(devicePixelRatio);
 
 // マテリアルを作成
 const material = new THREE.MeshPhysicalMaterial({
-  color: "3c94d7",
+  color: '3c94d7',
   metalness: 0.86,
   roughness: 0.37,
-  flatShading: true
-})
+  flatShading: false,
+});
 
 //ジオメトリ
-const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material)
-const mesh2 = new THREE.Mesh(new THREE.OctahedronGeometry(), material)
-const mesh3 = new THREE.Mesh(new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16), material)
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
+const mesh2 = new THREE.Mesh(new THREE.OctahedronGeometry(), material);
+const mesh3 = new THREE.Mesh(new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16), material);
 const mesh4 = new THREE.Mesh(new THREE.IcosahedronGeometry(), material);
 
 // 回転
@@ -51,6 +51,32 @@ mesh2.position.set(-1, 0, 0);
 mesh3.position.set(2, 0, -6);
 mesh4.position.set(5, 0, 3);
 
-scene.add(mesh1, mesh2, mesh3, mesh4)
+scene.add(mesh1, mesh2, mesh3, mesh4);
 
-renderer.render(scene, camera);
+// ライトを追加
+const directionalLight = new THREE.DirectionalLight('#ffffff', 4);
+directionalLight.position.set(0.5, 1, 0);
+scene.add(directionalLight);
+
+// リサイズ処理
+addEventListener('resize', () => {
+  // サイズのアップデート
+  sizes.width = innerWidth;
+  sizes.height = innerHeight;
+
+  // カメラのアップデート
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  //レンダラーのアップデート
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(devicePixelRatio);
+});
+
+// animation
+const animate = () => {
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+};
+
+animate();
